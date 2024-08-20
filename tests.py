73f977a2b2d28f8541771245dff8ae627be012e2
kira <kiracreate.or@gmail.com>
2024-08-20 16:55:01 +0300

    commit

--- tests.py
+++ tests.py
@@ -4,27 +4,27 @@ from main import BooksCollector
 
 class TestBooksCollector:
 
-    # 1)тест для метода add_new_book (добавление книги без жанра)
+    # 1)тест для метода add_new_book
     def test_add_new_book_have_no_genre_success(self, book_collection):
         book = 'Оно'
         book_collection.add_new_book(book)
         assert book_collection.get_book_genre(book) == ''
 
-    # 2)тест для метода add_new_book (добавление двух книг)
+    # 2)тест для метода add_new_book
     def test_add_new_book_adding_two_books_success(self, book_collection):
         books = ['Оно', 'Джек Ричер']
         for book in books:
             book_collection.add_new_book(book)
         assert len(book_collection.get_books_genre()) == 2
 
-    # 3)тест для метода add_new_book (добавление дубликата)
+    # 3)тест для метода add_new_book
     def test_add_new_book_duplicate_book_not_added(self, book_collection):
         books = ['Унесенные ветром', 'Унесенные ветром']
         for book in books:
             book_collection.add_new_book(book)
         assert len(book_collection.get_books_genre()) == 1
 
-    # 4) тест для метода add_new_book (невалидные значения)
+    # 4) тест для метода add_new_book
     @pytest.mark.parametrize('book',
                              ['', # 0 миволов
                               'Радости и горести знаменитой Молль Флендерс, которая родилась в Ньюгетской тюрьме', # 71 символ без пробелов
@@ -34,7 +34,7 @@ class TestBooksCollector:
         book_collection.add_new_book(book)
         assert len(book_collection.get_books_genre()) == 0
 
-    # 5) тест для метода set_book_genre (добавление жанра)
+    # 5) тест для метода set_book_genre
     def test_set_book_genre_added_genre_book_success(self, book_collection):
         book = 'Гарри Поттер и философский камень'
         genre = 'Фантастика'
@@ -42,7 +42,7 @@ class TestBooksCollector:
         book_collection.set_book_genre(book, genre)
         assert book_collection.get_book_genre(book) == genre
 
-    # 6) тест для метода set_book_genre (изменение жанра)
+    # 6) тест для метода set_book_genre
     def test_set_book_genre_changed_success(self, book_collection):
         book = 'Гарри Поттер и философский камень'
         genre = 'Фантастика'
@@ -52,7 +52,7 @@ class TestBooksCollector:
         book_collection.set_book_genre(book, new_genre)
         assert book_collection.get_book_genre(book) == new_genre
 
-    # 6) тест для метода set_book_genre (добавление несуществующего жанра)
+    # 6) тест для метода set_book_genre
     def test_set_book_genre_add_genre_is_not_list_not_added(self, book_collection):
         book = 'Гарри Поттер и философский камень'
         new_book_genre = 'Комикс'
@@ -60,20 +60,28 @@ class TestBooksCollector:
         book_collection.set_book_genre(book, new_book_genre)
         assert book_collection.get_book_genre(book) == ''
 
-    # 7) тест для метода get_books_with_specific_genre (вывод список книг с определённым жанром)
+    # 7) тест для метода get_book_genre
+    def test_get_book_genre_success(self, book_collection):
+        book = 'Гарри Поттер и философский камень'
+        genre = 'Фантастика'
+        book_collection.add_new_book(book)
+        book_collection.set_book_genre(book, genre)
+        assert book_collection.get_book_genre(book) == genre
+
+    # 8) тест для метода get_books_with_specific_genre
     def test_get_books_with_specific_genre_success(self, collection_five_book):
         assert collection_five_book.get_books_with_specific_genre('Детективы') == ['Шерлок Холмс']
 
-    # 8) тест для метода get_books_with_specific_genre (отсутствие книги определенного жанра)
+    # 9) тест для метода get_books_with_specific_genre
     def test_get_books_with_specific_genre_no_such_book_not_added(self, collection_five_book):
         assert len(collection_five_book.get_books_with_specific_genre('Комикс')) == 0
 
-    # 9) тест метода get_books_for_children (возвращает книги, которые подходят детям)
+    # 10) тест метода get_books_for_children
     def test_get_books_for_children_success(self, collection_five_book):
         children_book = collection_five_book.get_books_for_children()
         assert len(children_book) == 3 and children_book == ['Гарри Поттер и философский камень', 'Зелёная миля', 'Прислуга']
 
-    # 10) тест метода add_book_in_favorites (добавляет книгу в избранное)
+    # 11) тест метода add_book_in_favorites
     def test_add_book_in_favorites_add_book_success(self, book_collection):
         book = 'Гарри Поттер и философский камень'
         book_collection.add_new_book(book)
@@ -81,7 +89,7 @@ class TestBooksCollector:
         favorites = book_collection.get_list_of_favorites_books()
         assert len(favorites) == 1 and favorites[0] ==book
 
-    # 11) тест метода add_book_in_favorites (проверка на повторное добавление в избранное)
+    # 12) тест метода add_book_in_favorites
     def test_add_book_in_favorites_add_duplicate_book_not_added(self,book_collection):
         book = 'Гарри Поттер и философский камень'
         book_collection.add_new_book(book)
@@ -90,7 +98,7 @@ class TestBooksCollector:
         favorites = book_collection.get_list_of_favorites_books()
         assert len(favorites) == 1 and favorites[0] == book
 
-    # 12) тест метода delete_book_from_favorites (удаление книги из списка избранного)
+    # 13) тест метода delete_book_from_favorites
     def test_delete_book_from_favorites_deleted_book(self, book_collection):
         book = 'Гарри Поттер и философский камень'
         book_collection.add_new_book(book)
@@ -98,7 +106,7 @@ class TestBooksCollector:
         book_collection.delete_book_from_favorites(book)
         assert len(book_collection.get_list_of_favorites_books()) == 0
 
-    # 13) тест метода get_list_of_favorites_books (получение списка избранных книг)
+    # 14) тест метода get_list_of_favorites_books
     def test_get_list_of_favorites_books_got_list(self, book_collection):
         books = 'Гарри Поттер и философский камень'
         book_collection.add_new_book(books)
